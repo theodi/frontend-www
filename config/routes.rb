9b4854e651688af1c8ebc296f7869ef44e4f2379
pezholio <pezholio@gmail.com>
2013-10-15 09:21:27 +0100

    Tidy up routes (a bit)

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Www::Application.routes.draw do
   root :to => 'root#index'
   
   [:blog, :news, :jobs, :team, :case_studies, :courses, :creative_works, :procurement, :organizations, :nodes,
-    :consultation_response, :guides].each do |section|
-    get "#{section}", to: 'root#section', as: "#{section}_section", :section => section.to_s
-    get "#{section}/:slug", as: "#{section}_article", to: 'root#article'
+    :consultation_responses, :guides].each do |section|
+    section_slug = section.to_s.dasherize
+    get "#{section_slug}", to: 'root#section', as: "#{section}_section", :section => section_slug
+    get "#{section_slug}/:slug", as: "#{section}_article", to: 'root#article'
   end  
   
-  [:culture, "our-space", :dashboards, :membership, :certificates, "pg-certificate", "lunchtime-lectures", :newsroom].each do |page|
-    get "#{page}", as: "page", to: 'root#page', slug: page.to_s
+  [:culture, :our_space, :dashboards, :membership, :certificates, :pg_certificate, :lunchtime_lectures, :newsroom].each do |page|
+    slug = page.to_s.dasherize
+    get "#{slug}", as: "page", to: 'root#page', slug: slug
   end
   
   match "start-ups", to: 'root#section', as: "organizations_section", section: "organizations"
-  match "consultation-responses", to: 'root#section', as: "consultation_response_section", section: "consultation-response"
 
 end
