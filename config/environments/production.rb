dc44aa2bb83bfc83fd06e3c8d8317d754ab094f1
pezholio <pezholio@gmail.com>
2013-10-16 10:18:58 +0100

    Precompile Masonry JS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Www::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( imagesloaded.pkgd.min.js masonry.pkgd.min.js classie.js AnimOnScroll.js layout.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
