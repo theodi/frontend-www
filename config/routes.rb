d5ebea2ec501800a1b6c7d77ae41a7a5840de21d
Jeni Tennison <jeni@theodi.org>
2013-10-22 18:05:44 +0100

    for now, procurement page is just a page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Www::Application.routes.draw do
     get "#{item['url']}", as: "#{item['slug'].gsub('-','_')}_page", to: 'root#page', :slug => item['slug']
   end
   
-  [:blog, :news, :jobs, :team, :case_studies, :courses, :creative_works, :procurement, :start_ups, :nodes, :consultation_responses, :guides].each do |section|
+  [:blog, :news, :jobs, :team, :case_studies, :courses, :creative_works, :start_ups, :nodes, :consultation_responses, :guides].each do |section|
     section_slug = section.to_s.dasherize
     get "#{section_slug}", as: "#{section}_section", to: "root##{section}_list", :section => section_slug
 
