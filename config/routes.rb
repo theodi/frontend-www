e564ec6ffde50a6fb1106fa93edc09763dfb62e6
pezholio <pezholio@gmail.com>
2013-10-23 21:21:05 +0100

    Add culture to routes
    Must've knocked this off fixing conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Www::Application.routes.draw do
   
   get "culture/collection", as: "culture_collection", to: 'root#culture_collection'
 
-  [:blog, :news, :jobs, :team, :case_studies, :courses, :creative_works, :start_ups, :nodes, :consultation_responses, :guides, :events].each do |section|
+  [:blog, :news, :jobs, :team, :case_studies, :courses, :creative_works, :start_ups, :nodes, :consultation_responses, :guides, :events, :culture].each do |section|
     section_slug = section.to_s.dasherize
     get "#{section_slug}", as: "#{section}_section", to: "root##{section}_list", :section => section_slug
 
