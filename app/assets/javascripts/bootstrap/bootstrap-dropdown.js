0138e7d9daaac7cd3dd7b99223588c9b21245501
Jeni Tennison <jeni@theodi.org>
2013-10-25 19:30:44 +0100

    commenting out the thing that stops nav working in iPhone

--- app/assets/javascripts/bootstrap/bootstrap-dropdown.js
+++ app/assets/javascripts/bootstrap/bootstrap-dropdown.js
@@ -52,10 +52,10 @@
       clearMenus()
 
       if (!isActive) {
-        if ('ontouchstart' in document.documentElement) {
+//        if ('ontouchstart' in document.documentElement) {
           // if mobile we we use a backdrop because click events don't delegate
-          $('<div class="dropdown-backdrop"/>').insertBefore($(this)).on('click', clearMenus)
-        }
+//          $('<div class="dropdown-backdrop"/>').insertBefore($(this)).on('click', clearMenus)
+//        }
         $parent.toggleClass('open')
       }
 
