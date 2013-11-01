866b2b5a8c05def67c5e7224c05fe840eae9bc41
pezholio <pezholio@gmail.com>
2013-11-01 14:23:50 +0000

    Add precompile stuff to Jenkins
    There must be a better way to do this, but whevs

--- config/initializers/jenkins.rb
+++ config/initializers/jenkins.rb
@@ -3,5 +3,7 @@ if ENV['JENKINS']
   Www::Application.configure do
     config.assets.compile = false
     config.assets.digest = true
+    config.assets.precompile += %w( layout.js badge.css ie9.css ie8.css ie7.css ie6.css respond.min.js html5shiv.js
+                                    bootstrap/bootstrap-collapse.js modernizr.custom.js masonry.js )
   end
 end
