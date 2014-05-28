6b634e3078d732a78f5b37c4649c8884138e64d4
pezholio <pezholio@gmail.com>
2014-05-28 09:40:50 +0100

    Hound considered harmful

--- test/unit/application_helper_test.rb
+++ test/unit/application_helper_test.rb
@@ -10,14 +10,14 @@ class ApplicationHelperTest < ActionView::TestCase
 
   test 'page_title should work when only the section title is present' do
     @view_flow = ActionView::OutputFlow.new
-    content_for(:title) { '<a href='/foo'>Section title</a>'.html_safe }
+    content_for(:title) { '<a href="/foo">Section title</a>'.html_safe }
     assert_equal 'Section title | Open Data Institute', page_title
   end
 
   test 'page_title should work when both titles are present' do
     @view_flow = ActionView::OutputFlow.new
     content_for(:page_title) { 'Here is the main title' }
-    content_for(:title) { '<a href='/foo'>Section title</a>'.html_safe }
+    content_for(:title) { '<a href="/foo">Section title</a>'.html_safe }
     assert_equal 'Here is the main title | Section title | Open Data Institute', page_title
   end
 
