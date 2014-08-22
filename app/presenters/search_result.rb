3d0ac09759b01c4c74bd0c33172342aa0ea0108d
pezholio <pezholio@gmail.com>
2014-08-22 14:19:13 +0100

    Fix paths

--- app/presenters/search_result.rb
+++ app/presenters/search_result.rb
@@ -1,13 +1,28 @@
 class SearchResult
 
   PATH_LOOKUP = {
+    "consultation-response" => "consultation_responses_article_path",
     "blog" => "blog_article_path",
+    "start-up" => "start_ups_article_path",
+    "team" => "team_article_path",
+    "writers" => "team_article_path",
+    "artists" => "team_article_path",
+    "board" => "team_article_path",
+    "guide" => "guides_article_path",
+    "lunchtime-lecture" => "lunchtime_lectures_article_path",
+    "meetup" => "meetups_article_path",
+    "research-afternoon" => "research_afternoons_article_path",
+    "open-data-challenge-series" => "challenge_series_article_path",
+    "roundtable" => "roundtables_article_path",
+    "workshop" => "workshops_article_path",
+    "networking-events" => "networking_events_article_path",
+    "panel-discussions" => "panel_discussions_article_path",
     "news" => "news_article_path",
-    "partner-biography" => "partner_biographies_article_path",
-    "partner" => "partners_article_path",
-    "page" => "page_path",
-    "report" => "reports_article_path",
-    "networking-events" => "events_article_path"
+    "creative_work" => "culture_article_path",
+    "job" => "jobs_article_path",
+    "case_study" => "case_studies_article_path",
+    "node" => "nodes_article_path",
+    "course" => "courses_article_path"
   }
 
   def initialize(result)
@@ -31,10 +46,20 @@ class SearchResult
   end
 
   result_accessor :title, :es_score
-  details_accessor :slug, :format, :description
+  details_accessor :slug, :format, :description, :date, :course
 
   def path
-    Rails.application.routes.url_helpers.send(PATH_LOOKUP[format], slug)
+    if format == "course_instance"
+      Rails.application.routes.url_helpers.send("course_instance_path", course, date)
+    else
+      Rails.application.routes.url_helpers.send(path_name(format), slug)
+    end
   end
+
+  private
+
+    def path_name(format)
+      PATH_LOOKUP[format] || "page_path"
+    end
 
 end
---
+++ test/unit/search_result_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class SearchResultTest < ActiveSupport::TestCase
+
+  def setup
+    @result = {
+      "id"      => "http://contentapi.dev/space.json",
+      "web_url" => "http://www.dev/space",
+      "title"   => "Our Space",
+      "details" => {
+        "description" => "Here is some text",
+         "slug"       => "space",
+         "tag_ids"    => ["page", "odi"],
+         "format"     => "page",
+         "created_at" => "2012-12-03T19:59:19+00:00"
+        }
+    }
+  end
+
+  test "should present a result correctly" do
+    result = SearchResult.new(@result)
+
+    assert_equal result.title, "Our Space"
+    assert_equal result.description, "Here is some text"
+    assert_equal result.path, "/space"
+  end
+
+  test "should generate correct paths" do
+    {
+      "consultation-response" => "consultation-responses",
+      "blog" => "blog",
+      "start-up" => "start-ups",
+      "team" => "team",
+      "writers" => "team",
+      "artists" => "team",
+      "board" => "team",
+      "guide" => "guides",
+      "lunchtime-lecture" => "lunchtime-lectures",
+      "meetup" => "meetups",
+      "research-afternoon" => "research-afternoons",
+      "open-data-challenge-series" => "challenge-series",
+      "roundtable" => "roundtables",
+      "workshop" => "workshops",
+      "networking-events" => "networking-events",
+      "panel-discussions" => "panel-discussions",
+      "news" => "news",
+      "creative_work" => "culture",
+      "job" => "jobs",
+      "case_study" => "case-studies",
+      "node" => "nodes",
+      "course" => "courses"
+    }.each do |k,v|
+      details = {
+        "description" => "Here is some text",
+         "slug"       => "space",
+         "tag_ids"    => ["page", "odi"],
+         "format"     => k,
+         "created_at" => "2012-12-03T19:59:19+00:00"
+      }
+      result = SearchResult.new(@result.merge({"details" => details}))
+
+      assert_equal result.path, "/#{v}/space"
+    end
+  end
+
+end
