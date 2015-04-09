9b520b38ea60fd3950d3b0d2006438dd4fac4d52
Simon <svansintjan@gmail.com>
2015-04-09 18:37:37 +0700

    Filter tags based on AND, rather than OR

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -7,12 +7,26 @@ class TagController < ApplicationController
   def index
     # TODO: this needs to be made more robust.
     if params[:tag] && params[:tag] != ''
-      @joined_tags = params[:tag].split('/').join(',')
-      @options = { keyword: @joined_tags.to_s }
+      @joined_tags = params[:tag].split('/')
+      @options = { keyword: @joined_tags.join(',').to_s }
       begin
         @artefacts = content_api.with_tag('', @options).results
       rescue GdsApi::HTTPNotFound
         @artefacts = []
+      end
+      @artefacts = @artefacts.reject do |artefact|
+        doesnotexist = false
+
+        # The second that a joined tag isn't in tag_ids,
+        # we need to reject the artefact
+
+        # All the joined tags are represented.
+        @joined_tags.each do |tag|
+          unless artefact.tag_ids.include? tag
+            doesnotexist = true
+          end
+        end
+        doesnotexist
       end
     else
       @artefacts = []
