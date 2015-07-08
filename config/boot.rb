5fef779f3cc6352540cec6acf8662db2511b80fd
Giles Dring <gilesdring@gmail.com>
2015-07-08 12:15:25 +0100

    Dockerizing frontend-www
    Optionally require `resolv-replace` if `FIX_RESOLV`  variable set

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,6 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Fix to resolver issue - only active if FIX_RESOLV environment variable defined
+if defined? ENV['FIX_RESOLV'] then require 'resolv-replace' end
