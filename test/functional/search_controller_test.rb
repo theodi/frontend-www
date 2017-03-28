4e98fa17759d8483886f114aad612353c5f28f1d
James Smith <james@floppy.org.uk>
2017-03-28 12:47:18 +0100

    remove swearing in test

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class SearchControllerTest < ActionController::TestCase
 
   test "should show search results" do
-    stub_request(:get, "http://contentapi.dev/search.json?q=fuck&role=odi&page=1").
+    stub_request(:get, "http://contentapi.dev/search.json?q=space&role=odi&page=1").
       with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'Authorization'=>'Bearer overwritten on deploy', 'Content-Type'=>'application/json', 'User-Agent'=>'GDS Api Client v. 7.25.0'}).
       to_return(:status => 200, :body => load_fixture('search.json'), :headers => {})
 
-    get :perform, q: "fuck"
+    get :perform, q: "space"
     assert_response :ok
 
     doc = Nokogiri::HTML response.body
